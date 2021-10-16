e8e1ede9d96d0676443f52dd1f55c03c0e972efb
Tomas Pablo Korenblit <tomaskorenblit@gmail.com>
2021-10-16 12:54:42 -0300

    testing scroll

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./module.scss";
 
 function App() {
   return (
-    <div styles={{ height: "10px", overflowY: "scroll" }}>
+    <div>
       <Helmet>
         <meta
           name="viewport"
@@ -16,59 +16,91 @@ function App() {
         />
       </Helmet>
 
-      <Container className="floating">
-        <Row>
-          <span className="title-empty">unicope</span>
-        </Row>
-        <Row>
-          {" "}
-          <Nav
-            className="justify-content-center"
-            activeKey="/home"
-            sticky="top"
-            id="nav"
-          >
-            <Nav.Item>
-              <Nav.Link href="" className="nav-links">
-                Inicio
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                href="https://koren.empretienda.com.ar/"
-                target="_blank"
-                className="nav-links"
-              >
-                Tienda
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="" className="nav-links">
-                Contacto
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Row>
-      </Container>
+      <div styles={{ height: "10px", overflowY: "scroll", display:"none"}}>
+        <Container className="floating">
+          <Row>
+            <span className="title-empty">unicope</span>
+          </Row>
+          <Row>
+            {" "}
+            <Nav
+              className="justify-content-center"
+              activeKey="/home"
+              sticky="top"
+              id="nav"
+            >
+              <Nav.Item>
+                <Nav.Link href="" className="nav-links">
+                  Inicio
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  href="https://koren.empretienda.com.ar/"
+                  target="_blank"
+                  className="nav-links"
+                >
+                  Tienda
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="" className="nav-links">
+                  Contacto
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Row>
+        </Container>
 
-      <div id="stars"></div>
+        <Card className="card" id="card-sponsors">
+          <Card.Body>
+            <Card.Title className="card-title">
+              Estamos buscando sponsors
+            </Card.Title>
+            <Card.Text className="card-text">
+              Queremos contar con tu empresa u organización para llevar a cabo
+              nuestra visión.
+            </Card.Text>
+            <Button variant="primary" style={{ color: "white" }}>
+              ¡Contáctanos!
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="card" id="card-sponsors">
+          <Card.Body>
+            <Card.Title className="card-title">
+              Estamos buscando sponsors
+            </Card.Title>
+            <Card.Text className="card-text">
+              Queremos contar con tu empresa u organización para llevar a cabo
+              nuestra visión.
+            </Card.Text>
+            <Button variant="primary" style={{ color: "white" }}>
+              ¡Contáctanos!
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="card" id="card-sponsors">
+          <Card.Body>
+            <Card.Title className="card-title">
+              Estamos buscando sponsors
+            </Card.Title>
+            <Card.Text className="card-text">
+              Queremos contar con tu empresa u organización para llevar a cabo
+              nuestra visión.
+            </Card.Text>
+            <Button variant="primary" style={{ color: "white" }}>
+              ¡Contáctanos!
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+
+      {/* <div id="stars"></div>
       <div id="stars2"></div>
-      <div id="stars3"></div>
-
-      <Card className="card" id="card-sponsors">
-        <Card.Body>
-          <Card.Title className="card-title">
-            Estamos buscando sponsors
-          </Card.Title>
-          <Card.Text className="card-text">
-            Queremos contar con tu empresa u organización para llevar a cabo
-            nuestra visión.
-          </Card.Text>
-          <Button variant="primary" style={{ color: "white" }}>
-            ¡Contáctanos!
-          </Button>
-        </Card.Body>
-      </Card>
+      <div id="stars3"></div> */}
     </div>
   );
 }
